264d9737e3644eda7962ba824a9fdd369d093b64
Sathish A <sathish97rockz@gmail.com>
2017-10-05 09:09:25 +0530

    Update reshape.py

--- reshape.py
+++ reshape.py
@@ -1,35 +1,17 @@
 from PIL import Image
 from os import listdir
 
+dir = "/home/sam/PycharmProjects/classify/datasets/"
 
 def reshape(name):
     print("Reshaping " + name + "...")
     i = 0
-    for file in listdir("datasets/" + name):
+    for file in listdir(dir + name):
         if not file.startswith('.') and file != 'Thumbs.db':
             i = i + 1
             print(i)
-            images = Image.open("datasets/" + name + "/" + file)
+            images = Image.open(dir+ name + "/" + file)
             imResize = images.resize((200, 200), Image.ANTIALIAS)
-            imResize.save("datasets/" + name + "/" + file, 'JPEG', quality=90)
+            imResize.save(dir + name + "/" + file, 'JPEG', quality=90)
 
-
-def mov_file():
-    i = 0
-    for file in listdir("train/"):
-        if not file.startswith('.') and file != 'Thumbs.db':
-            i = i + 1
-            file_name_split = file.split('.')
-            file_name = file_name_split[0]
-            file_lab = file_name_split[1]
-            print(file_name, file_lab, i)
-            if file_name == 'cat':
-                save_dir = "cat/" + file_lab + ".jpg"
-            elif file_name == 'dog':
-                save_dir = "dog/" + file_lab + ".jpg"
-            images = Image.open("train/" + file)
-            imResize = images.resize((200, 200), Image.ANTIALIAS)
-            imResize.save(save_dir, 'JPEG', quality=90)
-
-
-mov_file()
+reshape("test")
